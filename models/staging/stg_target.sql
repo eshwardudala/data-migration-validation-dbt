158f09210b1d25c5643bbe951424e53e36ecc8de
Eshwar <84226229+eshwardudala@users.noreply.github.com>
2026-08-02 11:43:49 -0400

    Delete models directory

--- models/staging/stg_target.sql
+++
@@ -1,9 +0,0 @@
-
-
-{% set pk = var('primary_key', ['customerid']) %}
-{% set src = source('landing', 'migrated_customers') %}
-
-select
-    *,
-    {{ generate_row_hash(src, pk) }} as row_hash
-from {{ src }}
